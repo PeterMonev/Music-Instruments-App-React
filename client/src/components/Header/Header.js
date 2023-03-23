a228d653fcadded40a79156a30f65f38be5adead
PeterMonev <monevpeter@gmail.com>
2023-03-23 12:19:27 +0200

    Refactor Header CSS

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,15 +1,15 @@
 import { Link } from 'react-router-dom';
 
-import styles from '../Header/Header.module.css';
+import '../Header/Header.css';
 
 export const Header = () => {
     return (
         <header >
-     <h1 ><Link className={styles.title}to={'/'}>Music Instruments Shop</Link></h1>
+     <h1 ><Link className="title" eto={'/'}>Music Instruments Shop</Link></h1>
         <nav>
            <ul>
-              <li class="home"><Link to={'/'}>Home</Link></li>
-              <li><Link className={styles['a:hover']} to={'/catalog'}>Catalog</Link></li>
+              <li className="home"><Link to={'/'}>Home</Link></li>
+              <li><Link className='a:hover' to={'/catalog'}>Catalog</Link></li>
               <li><Link to={'/create'}>Create</Link></li>
               <li><Link to={'/login'}>Login</Link></li>
               <li><Link to={'/register'}>Register</Link></li>
