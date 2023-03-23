80b63d74e360517e85519e279ad2de85e814947d
PeterMonev <monevpeter@gmail.com>
2023-03-23 18:39:25 +0200

    Creating Email Validations

--- client/src/components/Register/Register.js
+++ client/src/components/Register/Register.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 
 import * as userServices from '../../services/userServices';
-import { inputValidator } from '../../utils/validations'
+import { inputValidator, passwordMatch } from '../../utils/validations'
 import '../Register/Register.css'
 
 export const Register = () => {
@@ -18,12 +18,16 @@ export const Register = () => {
     setuserData(state => ({...state, [event.target.name]: event.target.value}));
   }
 
-  function lengthValidator(event){
+  function lengthValidation(event){
     const tagName = event.target.name;
    
-    inputValidator(userData, tagName, setErrors);
-    console.log(errors);
+    inputValidator(userData, tagName, setErrors);    
+  };
+
+  function passwordMatchValidation(){
     
+    passwordMatch(userData, setErrors);
+    console.log(errors);
   }
    
   async function onSubmit(event) {
@@ -45,19 +49,28 @@ export const Register = () => {
         <h1>Register</h1>
 
         <label className="register-label" htmlFor="email">Email:</label>
-        <input onChange={onChange} value={userData.email} onBlur={lengthValidator} className="input-field" type="email"id="email" name="email" placeholder="peter@email.com"/>
+        <input onChange={onChange} value={userData.email} onBlur={lengthValidation} className="input-field" type="email"id="email" name="email" placeholder="peter@email.com"/>
 
         <label className="register-label" htmlFor="fullName">Full Name:</label>
-        <input onChange={onChange} value={userData.fullName} onBlur={lengthValidator} className="input-field" type="fullName" name="fullName" placeholder="Peter Johnson" />
+        <input onChange={onChange} value={userData.fullName} onBlur={lengthValidation} className="input-field" type="fullName" name="fullName" placeholder="Peter Johnson" />
 
         <label className="register-label" htmlFor="phone">Phone Number:</label>
-        <input onChange={onChange} value={userData.phone} onBlur={lengthValidator} className="input-field" type="phone" name="phone" placeholder="0123456789" />
+        <input onChange={onChange} value={userData.phone} onBlur={lengthValidation} className="input-field" type="phone" name="phone" placeholder="0123456789" />
 
         <label className="register-label" htmlFor="password">Password:</label>
-        <input onChange={onChange} value={userData.password} onBlur={lengthValidator} className="input-field" type="password" name="password" placeholder="*******" />
+        <input
+         onChange={onChange}
+          value={userData.password} 
+          onBlur={(e) => {
+            lengthValidation(e); 
+            passwordMatchValidation()}} 
+          className="input-field" 
+          type="password" 
+          name="password" 
+          placeholder="*******" />
 
         <label className="register-label" htmlFor="repeatPassword">Confirm Password:</label>
-        <input onChange={onChange} value={userData.repeatPassword} onBlur={lengthValidator} className="input-field" type="password" name="repeatPassword" placeholder="*******" />
+        <input onChange={onChange} value={userData.repeatPassword} onBlur={(e) => {lengthValidation(e); passwordMatchValidation()}} className="input-field" type="password" name="repeatPassword" placeholder="*******" />
 
         <input className='register-submit' type="submit" value="Register" />
 
