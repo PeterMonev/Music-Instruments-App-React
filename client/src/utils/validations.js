8ee2535c9cbea099eccd0b41e5a99a1893e5710d
PeterMonev <monevpeter@gmail.com>
2023-03-23 18:09:45 +0200

    Creating Password Validaton

--- client/src/utils/validations.js
+++ client/src/utils/validations.js
@@ -1,10 +1,23 @@
+export const emailValidator = (userData, setErrors) => {
+    
+}
 
-export const inputValidator = (userData, tagName, setErros) => {
+
+export const inputValidator = (userData, tagName, setErrors) => {
      
     if(userData[tagName].length < 4){
-       setErros(state => ({...state, [tagName]: `${tagName} should be 4 charaters long`}));
+       setErrors(state => ({...state, [tagName]: `${tagName} should be 4 charaters long`}));
     } else {
-       setErros(state => ({...state, [tagName]: null}))
+       setErrors(state => ({...state, [tagName]: null}))
     };
     
 };
+
+export const passwordMatch = (userData, setErrors) => {
+
+    if(userData.password !== userData.repeatPassword){
+        setErrors(state => ({...state, repeatPassword: `Password don't match`}));
+    } else {
+        setErrors(state => ({...state, repeatPassword: null}));
+    }
+}
