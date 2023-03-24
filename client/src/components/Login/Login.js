707ffa145c3d5aeee6573562873e99a4b5ed6aaf
PeterMonev <monevpeter@gmail.com>
2023-03-24 17:20:13 +0200

    Creating Validation on Login Page

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,24 +1,24 @@
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 import '../Login/Login.css';
 import * as userService from '../../services/userServices';
-import { useState } from 'react';
 
 export const Login = () => {
   const navigate = useNavigate();
-  const [errors, setErrors] = useState({});
+  const [error, setError] = useState('');
    
   async function onSubmit(event){
     event.preventDefault();
-
     const {email, password} = Object.fromEntries(new FormData(event.target));
 
     try {
       const user = await userService.login(email, password);
       navigate('/catalog');
-
+ 
     } catch (error) {
-      setErrors('Email or Password are incorrect');
+      console.log(error);
+      setError('Email or Password are incorrect!');
     }
   }
 
@@ -30,11 +30,12 @@ export const Login = () => {
 
         <label className="login-label" htmlFor="email">Email:</label>
         <input className="login-input" type="email" name="email" placeholder="peter@gmail.com" />
-       
+    
 
         <label className="login-label" htmlFor="login-pass">Password:</label>
         <input className="login-input" type="password" name="password" placeholder="******" />
 
+        {error && <p className="p-error"  >{error}</p>}
         <input className='submit-login' type="submit" value="Login" />
 
         <p className='login-container-p'>
