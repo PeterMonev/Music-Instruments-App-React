4385022a7ac8b473623ea0adc05b33b574ecbd3b
PeterMonev <monevpeter@gmail.com>
2023-04-02 16:20:35 +0300

    Creating Create service on CraeteComment

--- client/src/components/Details/CommentsList/CommentsCreate/CommentsCreate.js
+++ client/src/components/Details/CommentsList/CommentsCreate/CommentsCreate.js
@@ -1,13 +1,28 @@
-import '../CommentsCreate/CommentsCreate.css';
+import { useState } from "react";
+import "../CommentsCreate/CommentsCreate.css";
 
 export const CommentsCreate = ({ offerId }) => {
-    return (
-        <section className="comment-create-container">
-            <h4>Leave a comment:</h4>
-            <form >
-               <textarea ></textarea>
-               <input type="submit" value="Submit" />
-            </form>
-        </section>
-    )
-}   
+  const [comment, setCommnet] = useState("");
+
+  function onChange(event) {
+    setCommnet(event.target.value);
+  };
+
+  async function onSubmit(event){
+    event.preventDefault();
+
+    
+  }
+
+  console.log(comment);
+ 
+  return (
+    <section className="comment-create-container">
+      <h4>Leave a comment:</h4>
+      <form onSubmit={onSubmit}>
+        <textarea name="text" placeholder="Write youre comment..." onChange={onChange} />
+        <input type="submit" value="Submit" />
+      </form>
+    </section>
+  );
+};
