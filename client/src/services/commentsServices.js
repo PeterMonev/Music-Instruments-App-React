af9b1ff7853a1778397d4078bd48042970fe7613
PeterMonev <monevpeter@gmail.com>
2023-04-03 16:15:26 +0300

    Creating rerender function on comments

--- client/src/services/commentsServices.js
+++ client/src/services/commentsServices.js
@@ -3,4 +3,9 @@ import * as api from './api';
 export async function getCommnetsByOfferId(offerId){
     const response = await api.get(`/data/instrument/${offerId}/commnets`);
     return response;
+};
+
+export async function createComment(offerId, data){
+    const response = await api.post(`/data/instrument/${offerId}/commnets`, data);
+    return response;
 }
