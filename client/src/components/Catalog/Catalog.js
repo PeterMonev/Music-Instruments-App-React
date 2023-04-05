fcf8b8d1ce849dcf79cdda5ff17ed127ee2f4932
PeterMonev <monevpeter@gmail.com>
2023-04-05 17:36:04 +0300

    Creating Pagination

--- client/src/components/Catalog/Catalog.js
+++ client/src/components/Catalog/Catalog.js
@@ -4,6 +4,7 @@ import { getAll } from "../../services/instrumentServices";
 import "../Catalog/Catalog.css";
 import { OffersList } from "./OffersList/OffersList";
 import { Search } from "../Search/Search";
+import { Pagination } from "../Pagination/Pagination";
 
 const images = {
   firstImg: "/images/GuitarsCatalogPic.jpg",
@@ -15,18 +16,25 @@ const images = {
 export const Catalog = () => {
   const [offers, setOffers] = useState([]);
   const [searchData, setSearchData] = useState({});
-  
+  const [page, setPage] = useState(1);
+  const [instrumentsCount, setInstrumentsCount] = useState(1);
+
   useEffect(() => {
     (async () => {
-      const data = await getAll(searchData);
-     
+      let query = { limit: 6, page, ...searchData }
+      const data = await getAll(query);
       setOffers(data.instruments);
+      setInstrumentsCount(data.count);
     })();
-  }, [searchData]);
+  }, [searchData, page]);
 
   function searchHandler(newSearchData) {
     setSearchData(newSearchData);
-  }
+  };
+
+  function changePageHandler(newPage) {
+     setPage(newPage);
+  };
 
   return (
     <>
@@ -42,6 +50,9 @@ export const Catalog = () => {
         <Search searchHandler={searchHandler}/>
 
         <OffersList offers={offers} />
+
+        <Pagination changePageHandler={changePageHandler} page={page} instrumentsCount={instrumentsCount}/>
+
       </section>
     </>
   );
---
+++ client/src/components/Pagination/Pagination.js
@@ -0,0 +1,39 @@
+import '../Pagination/Pagination.css';
+
+export const Pagination = ({
+    changePageHandler,
+    page,
+    instrumentsCount}) => {
+
+    const currentPage = Number(page);
+    const pagesCount = Math.ceil(instrumentsCount / Number(6)) || 1;
+    
+    const paginateHandler = (event) => {
+        event.preventDefault();
+        changePageHandler(event.target.value)
+    }
+
+   return(
+    <section className="pagination-wrap">
+    <ul>
+        <li><button onClick={paginateHandler} value={1}>{'<<<'}</button></li>
+        <li><button onClick={paginateHandler} value={currentPage !== 1 ? currentPage - 1 : 1}>{'<'}</button></li>
+        {currentPage !== 1 &&
+            <>
+                <li><button className="unclickable">{currentPage - 1}</button></li>
+            </>
+        }
+
+        <li><button className="active">{currentPage}</button></li>
+
+        {pagesCount !== currentPage &&
+            <>
+                <li><button className="unclickable">{currentPage + 1}</button></li>
+            </>
+        }
+        <li><button onClick={paginateHandler} value={currentPage !== pagesCount ? currentPage + 1 : currentPage}> {'>'} </button></li>
+        <li><button onClick={paginateHandler} value={pagesCount}> {'>>>'}</button></li>
+    </ul>
+</section>
+   );
+};
