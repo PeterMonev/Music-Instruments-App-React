43b2aa149fe38c4f490115ca824db638c5cbf10e
PeterMonev <monevpeter@gmail.com>
2023-04-06 18:47:15 +0300

    ref

--- client/src/components/AboutUs/Contacts.js
+++ client/src/components/AboutUs/Contacts.js
@@ -6,11 +6,12 @@ export const Contact = () => {
 
               <div className='contact-info'>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas officiis ratione, eum hic iure enim. Assumenda, cupiditate facere? Dolorum quidem eaque tempora veniam provident labore dolores sapiente molestiae quod dolor?</p>
-                <p className='phoneNumber'>Phone Number: 555-66</p>
+                <p className='phoneNumber'>Phone Number: 555-666</p>
               </div>
 
               <div className='container-google-maps'>
-              <iframe width="100%" height="100%" id="gmap_canvas" src="https://maps.google.com/maps?q=burgas&t=&z=14&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+              <iframe width="100%" height="100%"   src="https://maps.google.com/maps?q=burgas&t=&z=14&ie=UTF8&iwloc=&output=embed"  
+                        loading="lazy"/>
               </div>
         </section>
     )
