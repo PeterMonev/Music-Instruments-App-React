8e2d4adb53ce556d95fe731d3b2649e8814bcd87
PeterMonev <monevpeter@gmail.com>
2023-05-23 08:45:27 +0300

    Refactoring Profile Page CSS

--- client/src/components/Profile/ProfileList/ProfileOffersCards/ProfileOffersCards.js
+++ client/src/components/Profile/ProfileList/ProfileOffersCards/ProfileOffersCards.js
@@ -8,7 +8,7 @@ export const ProfileOffersCards = ({ offer }) => {
         
         <img src={offer.imageUrl}  alt="offer.jpg"className="profile-offer-image"/>
         <h1 className='profile-offer-title'>{offer.title}</h1>
-        <p className='profile-offer-price'>{offer.price}$</p>
+        <p className='profile-offer-price'><span className='profile-price-span'>Price: </span>{offer.price}$</p>
       
         <Link to={`/catalog/${offer._id}`} className="profile-read-more-btn" >Read More <i className="fa-sharp fa-solid fa-angles-right fa-beat-fade"></i>  </Link>
         </li>
